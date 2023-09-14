464cadcded29e06f94e00a5aee3f45af968c2e70
Samira Jahan <samirashammi67@gmail.com>
2023-09-14 08:31:17 +0600

    task-5 commit

--- weather_report.php
+++ weather_report.php
@@ -4,32 +4,31 @@
     <title>Weather Report</title>
 </head>
 <body>
-    
-     <h2>Weather Report</h2>
-   <form action="weather_report.php" method="post">
-  <input type="text" name="temperature" placeholder="Temperature">
-  <input type="submit" value="Check"><br>
-</form>
+    <h2>Weather Report</h2>
+    <form method="post" action="">
+        Enter temperature: <input type="number" name="temperature" required><br><br>
+        <input type="submit" name="check" value="Check Weather">
+    </form>
 
-   <?php
+    <?php
+    if (isset($_POST['check'])) {
+        $temperature = intval($_POST['temperature']);
+        $message = "";
 
-$freezing_temperature = 0;
-$cool_temperature = 15;
-$warm_temperature = 25;
+        if ($temperature < 0) {
+            $message = "It's freezing!";
+        } elseif ($temperature >= 0 && $temperature < 15) {
+            $message = "It's cool.";
+        } elseif ($temperature >= 15 && $temperature < 25) {
+            $message = "It's mild.";
+        } elseif ($temperature >= 25 && $temperature < 35) {
+            $message = "It's warm.";
+        } else {
+            $message = "It's hot!";
+        }
 
-
-$temperature = $_POST['temperature'];
-
-
-if ($temperature < $freezing_temperature) {
-  echo "It's freezing!";
-} else if ($temperature < $cool_temperature) {
-  echo "It's cool.";
-} else {
-  echo "It's warm.";
-}
-
-?>
-
+        echo "<p>{$message}</p>";
+    }
+    ?>
 </body>
 </html>
